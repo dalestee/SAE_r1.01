15997dac16950f36ba22c53afd1ee76194e3e352
Caetano <84238339+dalestee@users.noreply.github.com>
2022-11-10 10:50:13 +0100

    Delete SAE_r1.01-main/SAE_r1.01-main directory

--- SAE_r1.01-main/SAE_r1.01-main/class_joueur.py
+++
@@ -1,8 +0,0 @@
-class Joueur:
-    def __init__(self,nom):
-        self.scoreT = 0
-        self.nom = nom
-        self.scoreM = 0
-        self.scoreP = 0
-        self.scoreD = 0
-        self.scoreA = 0
